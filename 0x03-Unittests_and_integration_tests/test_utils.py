deada38fad32d672d4eba1c3f3665a9c1f051916
Benson Gikonyo <muhiugikonyo@gmail.com>
2025-09-20 23:53:58 +0300

    Task 2-tested for mock http calls

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python3
-"""This module contains unit tests that tests for expected output given map and path"""
+"""This module contains unit tests """
 
 import unittest
 from parameterized import parameterized
 from utils import access_nested_map
+from utils import get_json
+from unittest.mock import patch, Mock
 
 class TestAccessNestedMap(unittest.TestCase):
     """Unit tests for utils.access_nested_map"""
@@ -26,3 +28,25 @@ class TestAccessNestedMap(unittest.TestCase):
         """tests for error given nested map and path"""
         with self.assertRaises(KeyError):
             access_nested_map(nested_map, path)
+            
+            
+
+class TestGetJson(unittest.TestCase):
+    """unit tests for utils.get_json"""
+    
+    @parameterized.expand ([
+        ("http://example.com", {"payload": True}),
+        ("http://holberton.io", {"payload": False}),
+    ])
+    
+    @patch("utils.request.get")
+    
+    def test_get_json(self, test_url, test_payload, mock_get):
+        mock_response = Mock()
+        mock_response.json.return_value = test_payload
+        mock_get.return_value = mock_response
+        
+        result = get_json(test_url)
+        
+        self.assertEqual(result, test_payload)
+        mock_get.assert_called_once_with(test_url)
