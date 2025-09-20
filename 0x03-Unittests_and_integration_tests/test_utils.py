36a9e354beaf664178542a9c5ddb8418dce55178
Benson Gikonyo <muhiugikonyo@gmail.com>
2025-09-21 00:04:43 +0300

    Task 3- Parameterize and patch

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -6,6 +6,7 @@ from parameterized import parameterized
 from utils import access_nested_map
 from utils import get_json
 from unittest.mock import patch, Mock
+from utils import memoize
 
 class TestAccessNestedMap(unittest.TestCase):
     """Unit tests for utils.access_nested_map"""
@@ -39,7 +40,7 @@ class TestGetJson(unittest.TestCase):
         ("http://holberton.io", {"payload": False}),
     ])
     
-    @unittest.mock.patch("utils.request.get")
+    @patch("utils.request.get")
     
     def test_get_json(self, test_url, test_payload, mock_get):
         mock_response = Mock()
@@ -50,3 +51,26 @@ class TestGetJson(unittest.TestCase):
         
         self.assertEqual(result, test_payload)
         mock_get.assert_called_once_with(test_url)
+
+
+
+class TestMemoize(unittest.TestCase):
+    """Unit tests for utils.memoize"""
+
+    def test_memoize(self):
+        class TestClass:
+            def a_method(self):
+                return 42
+
+            @memoize
+            def a_property(self):
+                return self.a_method()
+
+        with patch.object(TestClass, "a_method", return_value=42) as mock_method:
+            obj = TestClass()
+            result1 = obj.a_property
+            result2 = obj.a_property
+
+            self.assertEqual(result1, 42)
+            self.assertEqual(result2, 42)
+            mock_method.assert_called_once()
