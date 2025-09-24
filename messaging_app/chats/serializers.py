a8ee0f8d25a8a7000dcf693c502771fd1f0dc1b6
Benson Gikonyo <muhiugikonyo@gmail.com>
2025-09-24 23:20:15 +0300

    Added serializer validation error. Task 2 correction

--- messaging_app/chats/serializers.py
+++ messaging_app/chats/serializers.py
@@ -3,9 +3,12 @@ from rest_framework import serializers
 
 
 class UserSerializer(serializers.ModelSerializer):
-    first_name = serializers.CharField(required=True)
-    last_name = serializers.CharField(required=True)
-    email = serializers.CharField(required=True)
+    # first_name = serializers.CharField(required=True)
+    # last_name = serializers.CharField(required=True)
+    # email = serializers.CharField(required=True)
+    # password_hash = serializers.CharField(required=True)
+    # phone_number = serializers.CharField(required=True)
+    # role = serializers.CharField(required=True)
 
     class Meta:
         model = User
@@ -13,16 +16,23 @@ class UserSerializer(serializers.ModelSerializer):
 
 
 class MessageSerializer(serializers.ModelSerializer):
-    sender_id = UserSerializer(read_only=True)
+    sender_id = UserSerializer(source="sender_id", read_only=True)
     message_body = serializers.SerializerMethodField
 
     class Meta:
         model = Message
         fields = ["message_id", "sender_id", "message_body"]
 
+    def validate_message_body(self, value):
+        if not value or len(value.strip()) < 2:
+            raise serializers.ValidationError(
+                "Message body must be at least 2 characters long"
+            )
+        return value
+
 
 class ConversationSerializer(serializers.ModelSerializer):
-    participants_id = UserSerializer(many=True, read_only=True)
+    participants_id = UserSerializer(source="participants_id", read_only=True)
 
     class Meta:
         model = Conversation
