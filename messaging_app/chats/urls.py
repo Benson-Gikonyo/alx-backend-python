451f7871c7a123baddbd067c5a211d1e467134e3
Benson Gikonyo <muhiugikonyo@gmail.com>
2025-09-28 22:48:43 +0300

    Task 4 correction

--- messaging_app/chats/urls.py
+++ messaging_app/chats/urls.py
@@ -1,14 +1,13 @@
 from rest_framework import routers
 from django.urls import path, include
 from .views import ConversationViewSet, MessageViewSet
-from rest_framework_nested.routers import NestedSimpleRouter
 
 router = routers.DefaultRouter()
 router.register(r"conversations", ConversationViewSet, basename="conversation")
 # router.register(r"messages", MessageViewSet, basename="message")
 
 
-conversation_router = NestedSimpleRouter(
+conversation_router = routers.NestedSimpleRouter(
     router, r"conversations", lookup="conversations"
 )
 conversation_router.register(r"messages", MessageViewSet, basename="message")
